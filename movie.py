9dbc780d939a7f4e9a60718569064b2f2dde37dd
Dimitrije Jovanovic <20dimitrije02@gmail.com>
2023-05-09 22:06:11 +0200

    added date and time

---
+++ movie.py
@@ -0,0 +1,11 @@
+class Movie:
+    def __init__(self, title, href):
+        self.title = title
+        self.originalTitle = ""
+        self.runningTime = ""
+        self.releaseDate = ""
+        self.countryOfOrigin = ""
+        self.director = ""
+        self.cast = []
+        self.href = href
+        self.projectionTimes = []
--- vilinGrad.py
+++ vilinGrad.py
@@ -1,31 +1,30 @@
 import requests
+from movie import Movie
 from bs4 import BeautifulSoup
+from datetime import datetime
 
-
-class Movie:
-    def __init__(self, title):
-        self.title = title
-        self.originalTitle = ""
-        self.runningTime = ""
-        self.releaseDate = ""
-        self.countryOfOrigin = ""
-        self.director = ""
-        self.cast = []
-
-# TODO - Create URL generator for target day
-url = "http://vilingrad.rs/na-repertoaru?ScreeningDate=2023-05-05"
-
+def getProjectionTime(movieURL):
+    request = requests.get(movieURL)
+    html = BeautifulSoup(request.content, "html.parser")
+    dates = html.select('time[class*=date-time-field-time]')
+    times = []
+    for item in dates:
+        date = datetime.strptime(item['datetime'][:10], "%Y-%m-%d").date() # converting parsed string to date format
+        time = datetime.strptime(item['datetime'][11:] + ' ' + item['datetime'][10] + 'M', "%I:%M %p").time() # converting parsed string to time format
+        times.append(datetime.combine(date, time))
+    return times
+        
+url = "http://vilingrad.rs/na-repertoaru?ScreeningDate=" + datetime.today().strftime("%Y-%m-%d")
 request = requests.get(url)
 html = BeautifulSoup(request.content, "html.parser")
 results = html.find(class_="projection-page content-item")
-
 
 movies = []
 
 # movie titles - creating array of Movie objects
 titles = html.find_all("h1")
-for runningTime in titles:
-    movies.append(Movie(runningTime.text))
+for title in titles:
+    movies.append(Movie(title.text, title.findNext('a', href = True)['href']))
 
 # first two items are not movie titles
 movies.pop(0)
@@ -33,44 +32,39 @@ movies.pop(0)
 
 # original titles
 originalTitles = results.find_all('p', "text-field text-field-originalname")
-i = 0
-for originalTitle in originalTitles:
+for i, originalTitle in enumerate(originalTitles):
     movies[i].originalTitle = originalTitle.findNext("span", "value").text[1:]
-    i = i + 1
 
 # running time
 runningTimes = results.find_all('p', "numeric-field numeric-field-lengthminutes")
-i = 0
-for runningTime in runningTimes:
+for i, runningTime in enumerate(runningTimes):
     movies[i].runningTime = runningTime.findNext("span", "value").text
-    i = i + 1
 
 others = results.find_all(class_="numeric-field numeric-field-lengthminutes")
 
 #release date
-i = 0
-for runningTime in others:
-    movies[i].releaseDate = runningTime.findNext(class_="value").findNext(class_="value").text[1:-1]
-    i = i + 1
+for i, releaseDates in enumerate(others):
+   movies[i].releaseDate = releaseDates.findNext(class_="value").findNext(class_="value").text[1:-1]
 
 # country of origin
-i = 0
-for runningTime in others:
-    movies[i].countryOfOrigin = runningTime.findNext(class_="value").findNext(class_="value").findNext(class_="value").text[1:]
-    i = i + 1
+for i, countryOfOrigin in enumerate(others):
+    movies[i].countryOfOrigin = countryOfOrigin.findNext(class_="value").findNext(class_="value").findNext(class_="value").text[1:]
 
 # director
-i = 0
-for runningTime in others:
-    movies[i].director = runningTime.findNext(class_="value").findNext(class_="value").findNext(class_="value").findNext(class_="value").text[1:]
-    i = i + 1
+for i, directors in enumerate(others):
+    movies[i].director = directors.findNext(class_="value").findNext(class_="value").findNext(class_="value").findNext(class_="value").text[1:]
 
 # movie cast
-i = 0
-for runningTime in others:
-    movies[i].cast = runningTime.findNext(class_="value").findNext(class_="value").findNext(class_="value").findNext(class_="value").findNext(class_="value").text[1:]
-    i = i + 1
+for i, movieCast in enumerate(others):
+    movies[i].cast = movieCast.findNext(class_="value").findNext(class_="value").findNext(class_="value").findNext(class_="value").findNext(class_="value").text[1:]
 
-# stampanje
-for runningTime in movies:
-    print(runningTime.title + " | " + runningTime.originalTitle + " | " + runningTime.runningTime + " min | year " + runningTime.releaseDate + " | " + runningTime.countryOfOrigin + " | " + runningTime.director + " | " + runningTime.cast)
+# movie projection 
+for item in movies:
+    item.projectionTimes = getProjectionTime("http://vilingrad.rs/" + item.href)
+
+# printing
+for item in movies:
+    print(item.title + " | " + item.originalTitle + " | " + item.runningTime + " min | year " + item.releaseDate + " | " + item.countryOfOrigin + " | " + item.director + " | " + item.cast)
+    # print(item.title + " | " + item.href)
+    for time in item.projectionTimes:
+        print(time)
