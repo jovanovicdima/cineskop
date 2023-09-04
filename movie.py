9887373853b960b7a7826c5db47e1534f5750c03
Dimitrije Jovanovic <20dimitrije02@gmail.com>
2023-09-04 23:27:27 +0200

    add more info about tickets

--- movie.py
+++ movie.py
@@ -8,4 +8,19 @@ class Movie:
         self.director = ""
         self.cast = []
         self.href = href
-        self.projectionTimes = []
+        self.genre = ""
+        self.tickets = []
+
+class TicketInfo:
+    def __init__(self, projectionTime, projectionType, auditorium, status, ticketLink):
+        self.projectionTime = projectionTime
+        self.projectionType = projectionType
+        self.auditorium = auditorium
+        self.link = ticketLink
+        self.status = status
+        # status 0 - purchasable online, reservation available
+        # status 1 - purchasable online, reservation not available
+        # status 2 - not purchasable online, reservations availabe
+        # status 3 - only purchasable in person
+        # status 4 - reservations not available
+        # status 5 - no tickets left
